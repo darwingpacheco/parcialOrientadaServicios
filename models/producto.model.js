ca087866ce97dd99d0ee53e1d71cbe906d2f9b03
Darwin Gomez <darwingomez@wposs.com>
2024-04-12 17:43:16 -0500

    validaciones para el metodo update cuando los datos a modificar son los mismos que estan la base de datos

--- models/producto.model.js
+++ models/producto.model.js
@@ -80,16 +80,12 @@ export const updateProductoModel = async (id, data) => {
             return {success:"Producto no existente ", status:404, msg:"Product don't update"};
         }else if(!data.nombre || !data.detalle || !data.valor){
             return {error : "Todos los atributos del producto son obligatorios y el precio debe ser mayor a cero",status : 400, msg:"Product don't update"};
-        }else if(product[0].nombre == data.nombre){
-            await pg.connection.query(`update producto set nombre=$2, detalle=$3, valor=$4 where id_producto =$1`,[id, data.nombre, data.detalle, data.valor]);
-            return {success:"Producto actualizado exitosamente", status:200};
-        }else{
-            const product1 = await  pg.connection.query(`select * from producto where nombre = $1`,[data.nombre]);
-            if (product1[0]) {
-                return {error:"Ya existe un producto con ese nombre", status:409, msg: "Product don't update"};
+        }else if(product[0].id_producto == id){
+            if(product[0].nombre == data.nombre && product[0].detalle == data.detalle && product[0].valor == data.valor){
+                return {success:"Los datos ingresados son los mismos a los que ya estan almacenados", status:200};
             }
             await pg.connection.query(`update producto set nombre=$2, detalle=$3, valor=$4 where id_producto =$1`,[id, data.nombre, data.detalle, data.valor]);
-            return {success:"Producto actualizado exitosamente", status:200}
+            return {success:"Producto actualizado exitosamente", status:200};
         }
     }catch(e){
         return {error:"Error interno sel servidor", status:500, msg:"Product don't update"};
